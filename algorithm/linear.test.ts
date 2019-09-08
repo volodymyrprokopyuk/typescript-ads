f938ef84ff6f6822503f6ca7d756470658d1fba8
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2019-09-08 11:56:11 +0200

    feat(algorithm): add checkParentheses algorithm

--- algorithm/linear.test.ts
+++ algorithm/linear.test.ts
@@ -1,6 +1,6 @@
-import {reverse} from "algorithm/linear";
 import {DArray} from "dstructure/array";
 import {SList, DList} from "dstructure/list";
+import {reverse, checkParentheses} from "algorithm/linear";
 
 describe("reverse(iterable) returns reversed iterable", () => {
     describe.each([
@@ -26,4 +26,49 @@ describe("reverse(iterable) returns reversed iterable", () => {
             expect(reversed).toEqual(expectedIterable);
         });
     });
+});
+
+describe("checkParentheses(expression) returns the index of invalid parentheses or -1 otherwise", () => {
+    it("Should throw a TypeError on invalid supported parentheses", () => {
+        expect(() => checkParentheses("expression", "x")).toThrow(
+            new TypeError("Invalid parameter")
+        );
+    });
+    describe.each([
+        ["", -1],
+        ["x + y", -1],
+        ["()", -1],
+        ["({[]}),[()],{}", -1],
+        ["(x + y)", -1],
+        ["(x + y)[z]{w}", -1],
+        ["(x + {y + [z + w]})", -1],
+        ["(x + y", 5],
+        ["x + y)", 5],
+        ["(x + y]", 6],
+        ["(x + [y + {z + w)])", 16],
+        ["(x + [y + {z + w])", 16],
+        ["(x + [y + {z + w})])", 17],
+    ])("checkParentheses(%p) should be %p", (expression: any, expectedIndex) => {
+        it("Should return the index of invalid parentheses or -1 otherwise", () => {
+            const index = checkParentheses(expression);
+            expect(index).toBe(expectedIndex);
+        });
+    });
+    describe.each([
+        ["AB", "AB", -1],
+        ["AxB", "AB", -1],
+        ["A x + y B", "AB", -1],
+        ["A C x + y D B, C z D", "ABCD", -1],
+        ["A", "AB", 0],
+        ["Ax", "AB", 1],
+        ["xB", "AB", 1],
+    ])(
+        "checkParentheses(%p, %p) should be %p",
+        (expression: any, supportedParentheses: any, expectedIndex) => {
+            it("Should return the index of invalid parentheses or -1 otherwise", () => {
+                const index = checkParentheses(expression, supportedParentheses);
+                expect(index).toBe(expectedIndex);
+            });
+        }
+    );
 });
--- algorithm/linear.ts
+++ algorithm/linear.ts
@@ -1,14 +1,50 @@
-import {LStack} from "dstructure/stack";
+import {AStack, LStack} from "dstructure/stack";
 
+// O(n)
 export function reverse<T>(iterable: Iterable<T>): Iterable<T> {
-    const lstack = new LStack();
+    const stack = new LStack();
     for (const value of iterable) {
-        lstack.push(value);
+        stack.push(value);
     }
     const reversed = [];
-    while (lstack.length !== 0) {
-        const value = lstack.pop();
+    while (stack.length !== 0) {
+        const value = stack.pop();
         reversed.push(value);
     }
     return reversed as Iterable<T>;
+}
+
+// O(n)
+export function checkParentheses(
+    expression: string,
+    supportedParentheses = "()[]{}"
+): number {
+    const match = supportedParentheses.match(/.{2}/g);
+    if (match === null) {
+        throw new TypeError("Invalid parameter");
+    }
+    const openingToClosing = new Map(match.map((ps) => ps.split("")) as any);
+    const opening = new Set(openingToClosing.keys());
+    const closing = new Set(openingToClosing.values());
+    const stack = new AStack();
+    let index = 0;
+    for (const symbol of expression) {
+        if (opening.has(symbol)) {
+            stack.push(symbol);
+        } else if (closing.has(symbol)) {
+            if (stack.length === 0) {
+                return index;
+            }
+            const lastOpening = stack.pop();
+            const expectedClosing = openingToClosing.get(lastOpening);
+            if (symbol !== expectedClosing) {
+                return index;
+            }
+        }
+        ++index;
+    }
+    if (stack.length !== 0) {
+        return expression.length - 1;
+    }
+    return -1;
 }
